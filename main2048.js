37bd378510591375ea182325c082e9846bfe7ddb
Ruiheng Zhao <zhaoruiheng1996@gmail.com>
2016-07-15 01:09:54 -0400

    1

--- main2048.js
+++ main2048.js
@@ -1,1 +1,22 @@
+//  Game Data
+var board = new Array();
+var score = 0;
 
+$(document).ready(function() {
+    newgame();
+});
+
+function newgame(){
+	init();	
+}
+
+function init(){
+	for(var i=0; i<4; i++){
+		for(var j=0; i<4; j++){
+			var gridCell=$("#grid_cell_"+i+"_"+j);
+			gridCell.css('top', getPosTop(i, j));
+			gridCell.css('left', getPosLeft(i, j));
+		}	
+	}
+	
+}
