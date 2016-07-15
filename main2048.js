139a281672e4c6cb9b718dcce4c1a65696ec033e
Ruiheng Zhao <zhaoruiheng1996@gmail.com>
2016-07-15 12:40:52 -0400

    2

--- main2048.js
+++ main2048.js
@@ -7,16 +7,91 @@ $(document).ready(function() {
 });
 
 function newgame(){
+	// initialize play board
 	init();	
+	// Random generate 2 numbers
+	generateOneNumber();
+	generateOneNumber();
 }
 
-function init(){
-	for(var i=0; i<4; i++){
-		for(var j=0; i<4; j++){
-			var gridCell=$("#grid_cell_"+i+"_"+j);
-			gridCell.css('top', getPosTop(i, j));
-			gridCell.css('left', getPosLeft(i, j));
-		}	
+
+function init(){  // initialize play board
+	for (var i = 0; i < 4; i++) {
+		for(var j = 0; j < 4; j++) {
+			$("#grid_cell_"+i+"_"+j).css('top', getPosTop(i,j));
+			$("#grid_cell_"+i+"_"+j).css('left', getPosLeft(i,j));
+		}
 	}
-	
+
+	//  Create a 2D array
+	for (var i = 0; i < 4; i++) {
+		board[i]=new Array();
+		for(var j = 0; j < 4; j++) {
+			board[i][j] = 0;
+		}
+	}
+
+	updateBoardView();
 }
+
+function updateBoardView(){
+	$(".number_cell").remove();
+
+	for (var i = 0; i < 4; i++) {
+		for(var j = 0; j < 4; j++) {
+			$("#grid_container").append('<div class="number_cell" id="number_cell_'+i+'_'+j+'"></div>');
+			var theNumberCell = $('#number_cell_'+i+'_'+j);
+
+			if(board[i][j] == 0){
+				theNumberCell.css('width', '0px');
+				theNumberCell.css('height', '0px');
+				theNumberCell.css('top', getPosTop(i,j) + 50);
+				theNumberCell.css('left', getPosLeft(i,j) + 50);
+			}else{
+				theNumberCell.css('width', '100px');
+				theNumberCell.css('height', '100px');
+				theNumberCell.css('top', getPosTop(i,j));
+				theNumberCell.css('left', getPosLeft(i,j));
+				theNumberCell.css('background-color', getNumberBackgroundColor(board[i][j]));
+				theNumberCell.css('color', getNumberColor(board[i][j]));
+				theNumberCell.text(board[i][j]);
+			}
+		}
+	}
+}
+
+function generateOneNumber(){
+	if(nospace(board)){
+		return false;
+	}else{
+		//  Get a random position
+		var randx = parseInt(Math.floor(Math.random()*4));
+		var randy = parseInt(Math.floor(Math.random()*4));
+
+		while(1){
+			if(board[randx][randy] == 0){
+				break;
+			}
+			randx = parseInt(Math.floor(Math.random()*4));
+			randy = parseInt(Math.floor(Math.random()*4));
+		}
+
+		//  Get a random number
+		var randNumber = Math.random() < 0.5 ? 2 : 4;
+
+		//  Show the number on that position
+		board[randx][randy] = randNumber;
+		showNumberWithAnimation(randx, randy, randNumber);
+
+		return true;	
+	}
+}
+
+/*
+	for (var i = 0; i < 4; i++) {
+		for(var j = 0; j < 4; j++) {
+			
+		}
+	}
+
+*/
--- showanimation2048.js
+++ showanimation2048.js
@@ -1,1 +1,14 @@
+function showNumberWithAnimation(randx, randy, randNumber){
+	var numberCell = $('#number_cell_'+randx+'_'+randy);
 
+	numberCell.css('background-color', getNumberBackgroundColor(randNumber));
+	numberCell.css('color', getNumberColor(randNumber));
+	numberCell.text(randNumber);
+
+	numberCell.animate({
+		width:"100px",
+		height:"100px",
+		top:getPosTop(randx, randy),
+		left:getPosLeft(randx, randy)
+	}, 50);
+}
